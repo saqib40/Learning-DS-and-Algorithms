616dce1540b56c8d1a70cf676fb4e9bf2fbb492b
saqib hussain <hussainsakib44@gmail.com>
2023-04-20 20:04:02 +0530

    Update examples.py

--- Implementations In Python/Stack/examples.py
+++ Implementations In Python/Stack/examples.py
@@ -70,3 +70,102 @@ def checkBalanced(exp):
                 S.pop()
         i += 1
     return True if S.isEmpty() else False
+
+
+#Evaluating postfix using stack
+def perform(operator, operand1, operand2):
+    if operator == '+':
+        return int(operand1) + int(operand2)
+    elif operator == '-':
+        return int(operand1) - int(operand2)
+    elif operator == '*':
+        return int(operand1) * int(operand2)
+    elif operator == '/':
+        return int(operand1) / int(operand2)
+    elif operator == '**':
+        return int(operand1) ** int(operand2)
+
+def evaluatePostfix(exp):
+    myStack = Stack()
+    for x in exp:
+        if x != '+' and x != '-' and x != '*' and x != '/': #assuming no exponent operator ** but this along with other cases are to be handled
+            myStack.push(x)
+        else:
+            op2 = myStack.peek()
+            myStack.pop()
+            op1 = myStack.peek()
+            myStack.pop()
+            result = perform(x, op1, op2)
+            myStack.push(result)
+    return myStack.peek()
+
+#Evaluating prefix using stack
+def evaluatePrefix(exp):
+    myStack = Stack()
+    n = len(exp)
+    i = n-1
+    while i >= 0:
+        x = exp[i]
+        if x != '+' and x != '-' and x != '*' and x != '/': #assuming no exponent operator ** but this along with other cases are to be handled
+            myStack.push(x)
+        else:
+            op1 = myStack.peek()
+            myStack.pop()
+            op2 = myStack.peek()
+            myStack.pop()
+            result = perform(x, op1, op2)
+            myStack.push(result)
+        i -= 1
+    return myStack.peek()
+
+#Converting infix to postfix using stack (concatenation of string is time consuming with TC of O(n^2))
+def openingClosingP(x):
+    if x == '(' or x == '[' or x == '{':
+        return 'Opening Parentheses'
+    elif x == ')' or x == ']' or x == '}':
+        return 'Closing Parentheses'
+
+def operatorWeight(x):
+    if x == '+' or x == '-':
+        return 1
+    elif x == '*' or x == '/':
+        return 2
+    elif x == '**':
+        return 3
+
+
+def precedence(x,y):
+    a = operatorWeight(x)
+    b = operatorWeight(y)
+    if a > b:
+        return True
+    elif a < b:
+        return False
+    elif a == b: #When equal precedence, return true if they are left associative and false if they are right associative
+        if x == '**':
+            return False
+        else:
+            return True
+
+def infixToPostfix(exp):
+    myStack = Stack()
+    myString = ''
+    for x in exp:
+        if openingClosingP(x) == 'Opening Parentheses':
+            myStack.push(x)
+        elif openingClosingP(x) == 'Closing Parentheses':
+            while myStack.isEmpty() == False and openingClosingP(myStack.peek()) != 'Opening Parentheses':
+                myString += myStack.peek()
+                myStack.pop()
+            myStack.pop()
+        elif x != '+' and x != '-' and x != '*' and x != '/': #skipping exponentation cause it isn't single character as we are assuming here single digit numbers and operators
+            myString += x
+        else:
+            while myStack.isEmpty() == False and openingClosingP(myStack.peek()) != 'Opening Parentheses' and precedence(myStack.peek(),x):
+                myString += myStack.peek()
+                myStack.pop()
+            myStack.push(x)
+    while myStack.isEmpty() == False:
+        myString += myStack.peek()
+        myStack.pop()
+    return myString
